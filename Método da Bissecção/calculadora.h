1a7139bcf2e3a667dcc7cfb4a6adf79d9651ea59
Breno Klywer Olegario de Moura <breno.moura@alunos.ufersa.edu.br>
2023-12-11 15:41:13 -0300

    inicio

---
+++ Método da Bissecção/calculadora.h
@@ -0,0 +1,14 @@
+#ifndef CALCULADORA_H
+#define CALCULADORA_H
+
+#include <string>
+#include <unordered_map>
+
+bool ehOperador(char ch);
+bool ehOperando(char ch);
+int obterPrecedencia(char op);
+double aplicarOperador(char op, double operando1, double operando2);
+double avaliarPosfixa(const std::string &posfixa, const std::unordered_map<char, double> &valoresVariaveis);
+std::string infixParaPosfixa(const std::string &infixa);
+
+#endif // CALCULADORA_H
