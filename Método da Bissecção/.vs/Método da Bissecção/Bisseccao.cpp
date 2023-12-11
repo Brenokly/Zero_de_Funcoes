037a2b9f7ea91966d18b8b9c5162dc8a8711912f
Breno Klywer Olegario de Moura <breno.moura@alunos.ufersa.edu.br>
2023-12-11 15:00:02 -0300

    main

---
+++ Método da Bissecção/.vs/Método da Bissecção/Bisseccao.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "calculadora.h"
+using namespace std;
+
+int main()
+{
+  cout << "Digite a expressao matematica: ";
+  string entrada;
+  getline(cin, entrada);
+
+  cout << "Digite o valor de x: ";
+  double x;
+  cin >> x;
+
+  unordered_map<char, double> valoresVariaveis = {{'x', x}};
+
+  string posfixa = infixParaPosfixa(entrada);
+  cout << "Expressao posfixa: " << posfixa << endl;
+
+  double resultado = avaliarPosfixa(posfixa, valoresVariaveis);
+  cout << "Resultado da expressao: " << resultado << endl;
+
+  return 0;
+}
